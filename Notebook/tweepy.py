68da95d7f1df8962d01ade2858eb68eea46e910c
Alexander Sibagatov <q1178817@gmail.com>
2020-08-03 13:39:02 +0300

    up

---
+++ Notebook/tweepy.py
@@ -0,0 +1,26 @@
+import tweepy
+from textblob import TextBlob
+
+# Authenticate
+consumer_key= 'CONSUMER_KEY_HERE'
+consumer_secret= 'CONSUMER_SECRET_HERE'
+
+access_token='ACCESS_TOKEN_HERE'
+access_token_secret='ACCESS_TOKEN_SECRET_HERE'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# Retrieve Tweets
+public_tweets = api.search('Trump')
+
+
+for tweet in public_tweets:
+    print(tweet.text)
+    
+    # Perform Sentiment Analysis on Tweets
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+    print("")
